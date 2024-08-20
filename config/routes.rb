1350e73c1a75f5b0bc0f5cb63b927686ac9c38ee
marco <marcoogarbini@gmail.com>
2024-08-20 14:35:57 +0200

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :tools, only: [:new, :create, :index, :show, :destroy] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :categories, only: [:show, :index]
+  get "about_us", to: "pages#about_us"
+  get "profile/:id", to: "profile#show"
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
