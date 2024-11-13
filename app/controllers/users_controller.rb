72e6f6bff2dbb4a8e78d4fa0f9a70f66042dc9fd
marco garbini <marcoogarbini@gmail.com>
2024-11-13 12:02:21 +0100

    updating profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,14 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    # Display the user's profile
   end
 
-  # GET /users/:id/edit
   def edit
-    # Render the edit form for the user
   end
 
-  # PATCH/PUT /users/:id
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      redirect_to profile_path(@user), notice: 'User was successfully updated.'
     else
       render :edit
     end
@@ -21,12 +17,10 @@ class UsersController < ApplicationController
 
   private
 
-  # Find the user by ID
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Permit the user parameters including photo
   def user_params
     params.require(:user).permit(:first_name, :last_name, :phone_number, :email, :photo)
   end
